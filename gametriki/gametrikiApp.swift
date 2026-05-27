fe1079587c5d879742fb76c9ef49002a436ebcd7
Koder <59885735+koderhack@users.noreply.github.com>
2026-05-27 11:04:11 +0200

    Initial Commit

---
+++ gametriki/gametrikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  gametrikiApp.swift
+//  gametriki
+//
+//  Created by Przemysław Sikora on 27/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct gametrikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gametrikiTests/gametrikiTests.swift
@@ -0,0 +1,16 @@
+//
+//  gametrikiTests.swift
+//  gametrikiTests
+//
+//  Created by Przemysław Sikora on 27/05/2026.
+//
+
+import Testing
+
+struct gametrikiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
